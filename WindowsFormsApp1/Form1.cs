0902fa998c89a0cd21df7d93e0538ade42bb802f
Aviani DelKruz <tabehermann98@gmail.com>
2021-12-03 10:27:01 +0200

    THE

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,15 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
     }
 }
